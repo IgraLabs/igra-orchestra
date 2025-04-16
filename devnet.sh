96800e84f039dd3ca65309d513d80d8b36a12fbf
Vadim Konstantinov <vadim.konstantinov@gmail.com>
2025-04-16 22:51:49 +0300

    Refactor Docker Compose setup for environment flexibility
    Introduced support for multiple environments (local, stage, and prod) in devnet.sh and Docker Compose configurations. Added new environment variable handling, sample .env files, and separated network configurations. Adjusted service parameters and volumes for better scalability and consistency across environments.

--- devnet.sh
+++ devnet.sh
@@ -1,40 +1,59 @@
 #!/bin/bash
 set -e
 
+# Function to print help message
 print_help() {
-  echo "Usage: $0 [--dry-run] <compose-command> --all|<services>"
+  echo "Usage: $0 [--dry-run] [--env (local|stage|prod)] <compose-command> --all|--all-kaspa|<services>"
+  echo
+  echo "This script is a utility wrapper for Docker Compose that simplifies running, building,"
+  echo "and managing IGRA Devnet services in different environments (e.g., local, stage, prod)."
+  echo "It supports dynamic service selection and dry-run mode for testing commands."
   echo
   echo "--dry-run           Print only (don't run) docker command"
-  echo "--all               Same as 'traefik core workers kaspa-explorer'"
+  echo "--env               Define the environment to run services in:"
+  echo "                    (defaults to local if omitted)"
+  echo "   local            - devnet with KASPA node and miner on localhost"
+  echo "   stage            - devnet on stage server"
+  echo "   prod             - devnet on prod server"
   echo
   echo "Compose Commands:"
+  echo "  build            Build containers"
   echo "  up -d            Start services in background"
   echo "  down             Stop and remove containers"
   echo "  down -v          Stop and remove containers and volumes"
   echo "  logs             Show logs"
   echo "  logs -f          Follow logs"
   echo "  ps               List running containers"
+  echo "  config           Merge and print configs"
   echo "  stats            Report statistics"
-  echo "  build            Build containers"
   echo
-  echo "Service:           (combination of the following words)"
+  echo "--all               Same as 'traefik core workers'"
+  echo "--all-kaspa         Same as 'kaspa traefik core workers'"
+  echo
+  echo "Services:           (combination of the following words)"
   echo "  core             Same as 'execution-layer block-builder viaduct'"
-  echo "  execution-layer  Selects this service only"
+  echo "  execution-layer  Select this service only"
   echo "  block-builder    -''-"
   echo "  viaduct          -''-"
-  echo "  workers <ids>    Adds one or more dynamic workers (e.g. 0 1 2);"
+  echo "  workers <ids>    Add one or more dynamic workers (e.g. 0 1 2);"
   echo "                   defaults to all (0 1 2 3 4 5) if no ids given"
-  echo "  traefik          Adds traefik proxy"
-  echo "  kaspa-explorer   Selects all KASPA Explorer stack"
+  echo "  traefik          Add traefik proxy"
+  echo "  kaspa            KASPAD and kaspa-miner (e.g. or local env)"
+  echo "  kaspa-explorer   Select all KASPA Explorer stack"
   echo
   echo "Example:"
-  echo "  $0 up -d traefik core workers 1 2 kaspa-explorer"
+  echo "  $0 --env local up -d traefik core workers 1 2 kaspa-explorer"
 }
+
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    print_help
+    exit 0
+fi
 
 panic() {
   echo "❌ ERROR: $@" >&2
   echo >&2
-  print_help >&2
+  echo "Try \"$0 --help\"" >&2
   exit 1
 }
 
@@ -55,12 +74,22 @@ if [ "$1" == "--dry-run" ]; then
   shift
 fi
 
-# ---------- Parse Compose Command ----------
+# ---------- Environment           ----------
+ENV=local
+if [[ -n "$1" && "$1" == "--env" ]]; then
+  if [[ -n "$2" && "$2" =~ ^(local|stage|prod)$ ]]; then
+     ENV="$2"
+     shift 2
+  else
+     echo panic "Invalid or missing value for --env"
+  fi
+fi
 
+# ---------- Parse Compose Command ----------
 COMPOSE_CMD=()
 while [[ $# -gt 0 ]]; do
   case "$1" in
-    --all|core|traefik|workers|kaspa-explorer|execution-layer|block-builder|viaduct)
+    --all|--all-kaspa|block-builder|core|execution-layer|kaspa|kaspa-explorer|traefik|workers|viaduct)
       break
       ;;
     *)
@@ -76,7 +105,7 @@ fi
 
 # Validate supported COMPOSE_CMD
 joined_cmd="${COMPOSE_CMD[*]}"
-if [[ ! "$joined_cmd" =~ ^(up\ -d|down|down\ -v|logs|logs\ -f|ps|build|stats)$ ]]; then
+if [[ ! "$joined_cmd" =~ ^(build|up\ -d|down|down\ -v|logs|logs\ -f|ps|config|stats)$ ]]; then
   panic "Invalid docker compose command: '$joined_cmd'"
 fi
 
@@ -85,21 +114,20 @@ fi
 IS_CORE=
 IS_TRAEF=
 IS_WORKERS=
+IS_KASPA=
 IS_KEXPL=
 SERVICES=
 
-# Check if --all is given
-if [ "$1" == "--all" ]; then
+# Check if --all or --all-kaspa is given
+echo "*** DEBUG '$1'"
+if [[ "$1" == "--all" || "$1" == "--all-kaspa" ]]; then
+  [ "$1" == "--all-kaspa" ] && IS_KASPA=Y
   shift
-  if [[ $# -gt 0 ]]; then
-    panic "no params expected after --all"
-  fi
 
   # all services selected
   IS_CORE=Y
-  IS_TRAEF=Y
   IS_WORKERS=Y
-  IS_KEXPL=Y
+  IS_TRAEF=Y
 fi
 
 while [[ $# -gt 0 ]]; do
@@ -141,6 +169,11 @@ while [[ $# -gt 0 ]]; do
       done
       ;;
 
+    kaspa)
+      shift
+      IS_KASPA=Y
+      ;;
+
     kaspa-explorer)
       shift
       IS_KEXPL=Y
@@ -153,16 +186,34 @@ while [[ $# -gt 0 ]]; do
 done
 
 COMPOSE_FILES=
+[ -z ${IS_KASPA} ]   || COMPOSE_FILES+=" -f docker-compose.kaspad.local.yml"
 [ -z ${IS_CORE} ]    || COMPOSE_FILES+=" -f docker-compose.core.yml"
 [ -z ${IS_TRAEF} ]   || COMPOSE_FILES+=" -f docker-compose.traefik.yml"
 [ -z ${IS_WORKERS} ] || COMPOSE_FILES+=" -f docker-compose.rpc-workers.yml"
 [ -z ${IS_KEXPL} ]   || COMPOSE_FILES+=" -f docker-compose.kaspa-explorer.yml"
 [ -z "${COMPOSE_FILES}" ] && panic "No services selected"
 
+
+case "${ENV}" in
+  local)
+    env_file=".env.local"
+    COMPOSE_FILES="-f docker-compose.networks.local.yml ${COMPOSE_FILES}"
+    ;;
+  stage)
+    env_file=".env.stage"
+    COMPOSE_FILES+=" -f docker-compose.networks.stage.yml"
+    ;;
+  prod)
+    panic "PROD environment is not yet coded"
+    ;;
+esac
+
+[ -z "${env_file}" ] && panic "Unknown --env: ${ENV}"
+
 echo "Running:"
-echo "docker compose ${COMPOSE_FILES} ${joined_cmd} ${SERVICES}"
+echo "docker compose --env-file "${env_file}" ${COMPOSE_FILES} ${joined_cmd} ${SERVICES}"
 echo
 
-if [ -z "${DRY_RUN}" ]; then
-  docker compose ${COMPOSE_FILES} ${joined_cmd} ${SERVICES}
+if [[ ! "${DRY_RUN}" == "true" ]]; then
+  docker compose --env-file "${env_file}" ${COMPOSE_FILES} ${joined_cmd} ${SERVICES}
 fi
